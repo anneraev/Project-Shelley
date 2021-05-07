4ed891a347cec0fe0ede2ab88595a4215bf4e9e9
Anne Rae Vick <anneraevick@gmail.com>
2021-05-07 01:20:00 -0500

    created entity layer initialization

---
+++ source/ars-entity-lib/interface/IEntity_Layer.ts
@@ -0,0 +1,8 @@
+import IEntity from './IEntity';
+
+/**
+ * Represents the central repository of all Entities and the functions that manipulate them.
+ */
+export default interface IEntity_Layer {
+    entities: IEntity[];
+}
---
+++ source/ars-entity-lib/main.ts
@@ -0,0 +1,11 @@
+/// <reference types="rpgmakermv_typescript_dts" />
+
+import IEntity_Layer from './interface/IEntity_Layer';
+
+const entityLayer: IEntity_Layer = 
+{
+    entities: [],
+}
+
+console.log("entities Initialized!")
+console.log(entityLayer);
